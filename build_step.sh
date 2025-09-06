38bb95a291671a0e48acba796e9910a0188c9f0b
Leevi Kesänen <AD1229@student.jamk.fi>
2025-09-06 16:54:30 +0300

    10.7

--- build_step.sh
+++ build_step.sh
@@ -4,15 +4,12 @@ echo "Build script"
 
 echo "Installing dependencies"
 npm ci
-npx playwright install --with-deps
 
 echo "Linting"
 npm run eslint
 
-
 echo "Running unit tests"
 npm test
-npm run test:e2e  
 
 echo "Building production bundle"
 npm run build
